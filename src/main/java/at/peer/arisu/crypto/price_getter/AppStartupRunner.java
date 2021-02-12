4f2809c4d0a9798d6b6edc18bec85333d45828aa
Arisuuuuu <raphaelpeer56@gmail.com>
2021-02-12 13:27:13 +0100

    changed db schema

--- src/main/java/at/peer/arisu/crypto/price_getter/AppStartupRunner.java
+++ src/main/java/at/peer/arisu/crypto/price_getter/AppStartupRunner.java
@@ -1,20 +1,18 @@
 package at.peer.arisu.crypto.price_getter;
 
 import at.peer.arisu.crypto.price_getter.model.Crypto;
+import at.peer.arisu.crypto.price_getter.model.CryptoPrice;
+import at.peer.arisu.crypto.price_getter.repos.CryptoPriceRepository;
 import at.peer.arisu.crypto.price_getter.repos.CryptoRepository;
 import at.peer.arisu.crypto.price_getter.restclient.CryptoServiceGenerator;
 import at.peer.arisu.crypto.price_getter.restclient.PriceService;
-import at.peer.arisu.crypto.price_getter.restclient.ResponseModel;
-import okhttp3.OkHttpClient;
+import net.bytebuddy.dynamic.scaffold.MethodGraph;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import retrofit2.Call;
-import retrofit2.Callback;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,41 +22,35 @@ import java.util.*;
 public class AppStartupRunner implements ApplicationRunner {
 
     @Autowired
+    private CryptoPriceRepository cpr;
+
+    @Autowired
     private CryptoRepository cr;
+
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        List<Crypto> cryptoList = new LinkedList<>();
-
-        try(Scanner sc = new Scanner(new File("config.conf"))){
-            while(sc.hasNextLine()){
-                cryptoList.add(new Crypto(sc.nextLine().toLowerCase()));
-            }
-
-        }catch(IOException io){
-            System.err.println("Config File not found!\nApplication will shut down");
-            System.exit(1);
-        }
+        List<Crypto> cryptos = new LinkedList<>();
+        List<CryptoPrice> cryptoPrices = new LinkedList<>();
+        cryptos = cr.getAll();
 
 
-        for (Crypto c : cryptoList) {
+        for (Crypto c : cryptos) {
             PriceService service = CryptoServiceGenerator.createService(PriceService.class);
-            Call<Map<String, Map<String, Double>>> callSync = service.getPrice(c.getCurrency(), "eur");
-
+            Call<Map<String, Map<String, Double>>> callSync = service.getPrice(c.getName(), "eur");
             try {
                 Response<Map<String, Map<String, Double>>> response = callSync.execute();
                 Map<String, Map<String, Double>> price = response.body();
-                c.setPrice(Double.parseDouble(String.valueOf(price.get(c.getCurrency()).get("eur"))));
-                c.setTimestamp(new Date(System.currentTimeMillis()));
+                CryptoPrice newPrice = new CryptoPrice(c, (price.get(c.getName().toLowerCase()).get("eur")), new Date(System.currentTimeMillis()));
+                cryptoPrices.add(newPrice);
             }catch(Exception ex){
 
             }
 
         }
-        for (Crypto c: cryptoList) {
-            cr.save(c);
-        }
+        cpr.saveAll(cryptoPrices);
+
         System.exit(0);
     }
 }
--- src/main/java/at/peer/arisu/crypto/price_getter/repos/CryptoRepository.java
+++ src/main/java/at/peer/arisu/crypto/price_getter/repos/CryptoRepository.java
@@ -2,7 +2,13 @@ package at.peer.arisu.crypto.price_getter.repos;
 
 import at.peer.arisu.crypto.price_getter.model.Crypto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface CryptoRepository extends JpaRepository<Crypto, String> {
+import java.util.List;
+
+public interface CryptoRepository extends JpaRepository<Crypto, Integer> {
+
+    @Query("select c from Crypto c")
+    List<Crypto> getAll();
 
 }
