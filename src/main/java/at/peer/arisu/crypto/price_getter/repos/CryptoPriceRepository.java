d49223f43294238e8659d9662739e256bcb61d93
Arisuuuuu <raphaelpeer56@gmail.com>
2021-02-12 13:48:42 +0100

    changed pk of cryptoPrice

--- src/main/java/at/peer/arisu/crypto/price_getter/repos/CryptoPriceRepository.java
+++ src/main/java/at/peer/arisu/crypto/price_getter/repos/CryptoPriceRepository.java
@@ -3,6 +3,6 @@ package at.peer.arisu.crypto.price_getter.repos;
 import at.peer.arisu.crypto.price_getter.model.CryptoPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CryptoPriceRepository extends JpaRepository<CryptoPrice, String> {
+public interface CryptoPriceRepository extends JpaRepository<CryptoPrice, Integer> {
 
 }
